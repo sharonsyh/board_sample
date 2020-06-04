50b4645764085f61c10decc7770fa06e39e8d7cf
20140339 <kva231@naver.com>
2020-06-04 22:09:36 +0900

    데이터베이스 구축

--- WebContent/login.jsp
+++ WebContent/login.jsp
@@ -46,10 +46,10 @@
 				<form action="loginAction.jsp" method="post">
 					<h3 style="text-align: center;">로그인 화면</h3>
 					<div class="form-group">
-						<input type="text" class="form-control" placeholder="아이디" name="nameID"  maxlength="20">
+						<input type="text" class="form-control" placeholder="아이디" name="id"  maxlength="20">
 					</div>
 					<div class="form-group">
-						<input type="password" class="form-control" placeholder="비밀번호" name="namePassword"  maxlength="20">
+						<input type="password" class="form-control" placeholder="비밀번호" name="pw"  maxlength="20">
 					</div>
 					<input type="submit" class="btn btn-primary form-control" value="로그인">
 				</form>
---
+++ src/user/User.java
@@ -0,0 +1,42 @@
+package user;
+
+public class User {
+	
+	private String id;
+	private String pw;
+	private String name;
+	private String gender;
+	private String email;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+		
+}
